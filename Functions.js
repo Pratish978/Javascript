fb0517e5985106f5a1ef4d3eef40a8bcbfd6dae9
Pratish <bhonglepratish@gmail.com>
2026-06-14 15:17:29 +0530

    Initial commit

---
+++ Functions.js
@@ -0,0 +1,30 @@
+//Functions
+function MyName(){
+console.log("Pratish");
+}
+
+MyName();
+
+function add(num1 ,num2){ //Parameters
+    console.log(num1+num2);
+}
+
+add(3,4);//arguments
+
+
+function add(num1 ,num2){ //Parameters
+    let result=num1+num2;
+    return result;
+}
+
+const result=add(3,4)
+console.log(result);
+
+function LoginUser(userName){
+    if(userName===undefined){
+        console.log("Please Enter your username");
+    }
+    return `${userName} just logged in`
+}
+
+console.log(LoginUser("Pratish"));
