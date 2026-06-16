e89fb5f0dc5fc4e4113c3a1323f4c6bff903fbd3
Pratish <bhonglepratish@gmail.com>
2026-06-16 23:50:18 +0530

    Initial commit

---
+++ Basics/While.js
@@ -0,0 +1,25 @@
+//While
+
+let i=0;
+while (i<=10) {
+    console.log(`value of index is ${i}`);
+    i=i+2;
+}
+
+let myArray=["Flash","Batman","Superman"];
+
+let i=0
+
+while(i< myArray.length){
+    console.log(`value is ${myArray[i]}`);
+    i=i+1;
+}
+
+//Do While Loop
+
+let score=1;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
---
+++ DOM/DomSelector.js
@@ -0,0 +1,47 @@
+/*
+
+//Do this inside console of index.html
+
+
+document.getElemntById
+
+>document.getElemntById('title').id //Gives id
+<title
+
+>document.getElemntById('title').className //Gives class name
+<heading
+
+>document.getElemntById('title').getAttribute(id)
+<title
+
+>document.getElemntById('title').getAttribute(class)
+<heading
+
+>document.getElementById('title').setAttribute('class','test');
+
+>document.getElementById('title')
+< <h1 id="title" class="test">DOM learning</h1>
+
+>const title = document.getElementById('title')
+
+>title
+< <h1 id="title" class="test">DOM learning</h1>
+
+>title.style.backgroundColor ="red"
+<'red'
+
+>title.style.padding ="15px"
+<'15px'
+
+>title.style.borderRadius ="15px";
+<'15px'
+
+>title.textContent
+<'DOM learning Test Text'
+
+>title.innerHTML
+<'DOM learning'
+
+>title.innerText
+<DOM learning <spam style="display:none";>  Test Text</spam>
+*/
