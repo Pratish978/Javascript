6599b523ec36be5336695055aa1b909f9557781d
Pratish <bhonglepratish@gmail.com>
2026-06-17 23:13:12 +0530

    Initial commit

--- DOM/DomSelector.js
+++ DOM/DomSelector.js
@@ -44,4 +44,35 @@ document.getElemntById
 
 >title.innerText
 <DOM learning <spam style="display:none";>  Test Text</spam>
+
+<document.querySelector('h2') //Gives only first h2
+><h2>lorem ipsum dolor sit</h2>
+
+<document.querySelector('#title')
+><h1 id="title" class="heading">DOM learning <spam style="display:none";>  Test Text</spam></h1>
+
+<document.querySelector('.heading')
+><h1 id="title" class="heading">DOM learning <spam style="display:none";>  Test Text</spam></h1>
+
+<document.querySelector('input[type="password"]')
+><input type="password" name="" id="">
+
+<const myUl=document.querySelector('ul')
+
+<myUl.querySelector('li')
+><li></li>
+
+<const turnGreen=myUl.querySelector('li')
+
+<turnGreen.style.backgroundColor="green"
+>green
+
+
+<document.querySelectorAll('li')
+>nodelist(3) [li,li,li]
+
+<const tempList=document.querySelectorAll('li')
+
+<tempList[0].style.color="red"
+>red
 */
