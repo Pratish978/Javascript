d14437f59ae5d4e6d3c83a1bc26fe060f8bc78e0
Pratish <bhonglepratish@gmail.com>
2026-06-23 22:26:44 +0530

    first commit

---
+++ Async/async.js
@@ -0,0 +1,22 @@
+/*
+
+Default js behaviour :-
+Synchronous.
+Single Thread.
+
+Execution Context:-
+Execute one line of code at a time
+Each operation waits for the last one to complete before executing .
+Sibgle Thread
+
+//Blocking code vs Non-Blocking code:-
+
+1.Blocking code:-
+  Block the flow of program.
+  Read file sync.(Synchronous)
+
+2.Non-Blocking code
+  Does not block execution.
+  Read file Async.(ASynchronous)
+
+*/
